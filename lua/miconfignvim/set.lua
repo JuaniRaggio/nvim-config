c3ba2b823698e555590655a92fefdc6b7f08dab5
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2024-12-20 17:05:51 -0300

    mods

--- lua/miconfignvim/set.lua
+++ lua/miconfignvim/set.lua
@@ -2,8 +2,8 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 
 vim.opt.tabstop = 8
-vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.softtabstop = 8
+vim.opt.shiftwidth = 8
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
