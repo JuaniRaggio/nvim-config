ae0ec9246052656ac8ab65293e4ba905e40f9b70
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-05 17:40:08 -0300

    Adding none-ls, some Logs were popping up

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,12 +1,13 @@
 return {
-	"nvimtools/none-ls.nvim",
-	config = function()
-		local null_ls = require("null-ls")
+  "nvimtools/none-ls.nvim",
 
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-			},
-		})
-	end,
+  config = function()
+    local null_ls = require("null-ls")
+
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+      },
+    })
+  end,
 }
