edcd19932acbbff70d35149f85e15d13507f1cf1
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-07 02:30:55 -0300

    Changing cursor mode and theme

--- lua/miconfignvim/set.lua
+++ lua/miconfignvim/set.lua
@@ -27,6 +27,12 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 -- Block cursor even in insert mode
--- vim.opt.guicursor =
--- "n-v-c-i:block,i-ci-ve:block25,r-cr:block20,o:block50,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,sm:block-blinkwait175-blinkoff150-blinkon175"
+vim.opt.guicursor = {
+  "n-v-c-i:block",
+  "i-ci-ve:block25",
+  "r-cr:block20",
+  "o:block50",
+  "a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor",
+  "sm:block-blinkwait175-blinkoff150-blinkon175",
+}
 vim.opt.cursorline = true
