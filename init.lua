04bd4276f55e4a8d4e2c259ca53a7d984ad12037
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-17 18:05:10 -0300

    Changing relative line number color

--- init.lua
+++ init.lua
@@ -4,14 +4,6 @@ vim.opt.shellcmdflag = "-ic"
 
 local opts = { noremap = true, silent = true }
 
--- This is not that usefull since you can run commands using :!<cmd>
--- vim.api.nvim_set_keymap(
---   "n",
---   "<leader>h",
---   ":rightbelow vnew | vertical resize 42 | terminal<CR>",
---   { noremap = true, silent = true }
--- )
-
 vim.api.nvim_set_keymap(
   "n",
   "gd",
@@ -23,14 +15,17 @@ require("miconfignvim")
 require("miconfignvim.lazy")
 
 vim.cmd([[
-  colorscheme tokyonight-storm
+  colorscheme tokyonight
   highlight TermNormal guibg=NONE guifg=NONE
   highlight TermNormalNC guibg=NONE guifg=NONE
   highlight Normal guibg=NONE ctermbg=NONE
-  highlight LineNr guibg=NONE
   highlight SignColumn guibg=NONE
   highlight StatusLine guibg=NONE
   highlight VertSplit guibg=NONE
-  highlight! link @variable NONE
+  highlight LineNr       guifg=#93CEED gui=NONE
+  highlight CursorLineNr guifg=#FFFFFF gui=bold
+  highlight LineNrAbove  guifg=#93CEED gui=NONE
+  highlight LineNrBelow  guifg=#93CEED gui=NONE
   highlight! link @type NONE
+  highlight! link @variable NONE
 ]])
