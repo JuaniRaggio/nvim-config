f0a0621a43b23ff43b16b20c7cc44c373b6d27a7
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-04-23 00:03:22 -0300

    Quiet theme

--- init.lua
+++ init.lua
@@ -15,7 +15,7 @@ require("miconfignvim")
 require("miconfignvim.lazy")
 
 vim.cmd([[
-  colorscheme tokyonight-storm
+  colorscheme quiet
   highlight TermNormal guibg=NONE guifg=NONE
   highlight TermNormalNC guibg=NONE guifg=NONE
   highlight Normal guibg=NONE ctermbg=NONE
