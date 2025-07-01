f1f0ef3611b0a27fa6714bf2cca84c2204cd993e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2025-07-01 01:56:02 -0300

    Oil FM and 4 Tab Indentation for working on groups

--- after/plugin/keymaps.lua
+++ after/plugin/keymaps.lua
@@ -1,4 +1,5 @@
 -- Keymaps
+vim.keymap.set("n", "<leader>e", ":Oil<CR>")
 vim.keymap.set("n", "<leader>ff", ":Telescope find_files<CR>")
 vim.keymap.set("n", "<leader>/", ":Telescope live_grep<CR>")
 vim.keymap.set("n", "<leader>th", ":Telescope colorscheme<CR>")
