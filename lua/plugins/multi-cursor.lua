d0740b3380626d6372ffae1610aa65b87917587e
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-13 11:32:59 -0300

    update multi-cursor commands

--- lua/plugins/multi-cursor.lua
+++ lua/plugins/multi-cursor.lua
@@ -1,3 +1,10 @@
 return {
   "mg979/vim-visual-multi",
+  init = function()
+    vim.g.VM_maps = {
+      ["Add Cursor Down"] = "gzj",
+      ["Add Cursor Up"] = "gzk",
+      ["Find Under"] = "gzm",
+    }
+  end,
 }
