e4472a36da98dec980fd78e69037a578b9e79db8
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-17 02:13:39 -0300

    multicursor emacs km + harpoon km

--- lua/plugins/multi-cursor.lua
+++ lua/plugins/multi-cursor.lua
@@ -7,4 +7,15 @@ return {
       ["Find Under"] = "gzm",
     }
   end,
+  config = function()
+    vim.keymap.set("n", "gzw", function()
+      vim.cmd([[call vm#commands#add_cursor_at_pos(0)]])
+      vim.cmd("normal! w")
+    end, { desc = "Add cursor at next word" })
+
+    vim.keymap.set("n", "gzW", function()
+      vim.cmd([[call vm#commands#add_cursor_at_pos(0)]])
+      vim.cmd("normal! W")
+    end, { desc = "Add cursor at next WORD" })
+  end,
 }
