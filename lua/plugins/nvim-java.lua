41d8f8f59d8cf959c9b0fd22f8be85266636d13b
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-01-25 01:02:54 -0300

    fix(nvim-java): configuration for lsp

--- lua/plugins/nvim-java.lua
+++ lua/plugins/nvim-java.lua
@@ -25,7 +25,7 @@ return {
           enable = true,
         },
         jdk = {
-          auto_install = false,
+          auto_install = true,
         },
         notifications = {
           dap = true,
