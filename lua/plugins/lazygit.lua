4e6201d93f4eaeb96e9bdad734ada6eae329f448
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-02-17 01:36:08 -0300

    fix lazygit configuration so that it occupies fullscreen

--- lua/plugins/lazygit.lua
+++ lua/plugins/lazygit.lua
@@ -5,6 +5,10 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
   },
+  config = function()
+    vim.g.lazygit_floating_window_use_plenary = 0
+    vim.g.lazygit_floating_window_scaling_factor = 1
+  end,
   keys = {
     { "<leader>gg", "<cmd>LazyGit<cr>", desc = "Open LazyGit" },
   },
