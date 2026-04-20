a181e1cfa072ed264ddd98cdc8895d0c67a4b0b8
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-04-20 01:54:06 -0300

    fe for errors

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -2,7 +2,7 @@ return {
   "folke/trouble.nvim",
   cmd = "Trouble",
   keys = {
-    { "<leader>xx", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (project)" },
+    { "<leader>fe", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (project)" },
     { "<leader>xd", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Diagnostics (buffer)" },
     { "<leader>xq", "<cmd>Trouble qflist toggle<cr>", desc = "Quickfix list" },
   },
