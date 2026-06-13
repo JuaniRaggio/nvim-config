a8d38cf42359c15167a164b49950591bb3fb5c76
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-06-13 16:44:00 -0300

    theme: bm

--- lua/plugins/black-metal.lua
+++ lua/plugins/black-metal.lua
@@ -3,9 +3,6 @@ return {
   lazy = false,
   priority = 1000,
   config = function()
-    -- Tema mono-acento (negro + grises + un solo color). NO se le aplica el
-    -- esquema "minimal" porque ya nace minimalista; los acentos de tokyonight/
-    -- everforest lo arruinarian.
     require("black-metal").setup({
       theme = "bathory", -- bathory | burzum | gorgoroth | immortal | marduk | mayhem | nile | venom | darkthrone | emperor | ...
       variant = "dark",
@@ -16,6 +13,5 @@ return {
         keywords = "none",
       },
     })
-    -- Para activarlo: :colorscheme bathory  (nombre de la banda, no "black-metal")
   end,
 }
