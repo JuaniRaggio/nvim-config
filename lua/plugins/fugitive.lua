705357f4ef914b4db87b6888a6dcfcdf8bd0646f
JuaniRaggio <garciavautrinraggiojuanignacio@gmail.com>
2026-06-17 19:29:06 -0300

    fix: confortable commands

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,27 +1,27 @@
 return {
-	"tpope/vim-fugitive",
-	cmd = "Git",
-	keys = {
-		-- Git status (interactive)
-		{ "<leader>gs", "<cmd>Git<cr><cmd>only<cr>", desc = "Git status" },
+        "tpope/vim-fugitive",
+        cmd = "Git",
+        keys = {
+                -- Git status (interactive)
+                { "<leader>gg",  "<cmd>Git<cr><cmd>only<cr>",   desc = "Git status" },
 
-		-- Commits
-		{ "<leader>gc", "<cmd>Git commit<cr>", desc = "Git commit" },
-		{ "<leader>gca", "<cmd>Git commit --amend<cr>", desc = "Git commit amend" },
+                -- Commits
+                { "<leader>gc",  "<cmd>Git commit<cr>",         desc = "Git commit" },
+                { "<leader>gca", "<cmd>Git commit --amend<cr>", desc = "Git commit amend" },
 
-		-- Push/Pull
-		{ "<leader>gp", "<cmd>Git push<cr>", desc = "Git push" },
-		{ "<leader>gP", "<cmd>Git pull<cr>", desc = "Git pull" },
+                -- Push/Pull
+                { "<leader>gP",  "<cmd>Git push<cr>",           desc = "Git push" },
+                { "<leader>gp",  "<cmd>Git pull<cr>",           desc = "Git pull" },
 
-		-- Merge conflicts (3-way merge)
-		{ "<leader>gm", "<cmd>Gdiffsplit!<cr>", desc = "3-way merge (conflicts)" },
+                -- Merge conflicts (3-way merge)
+                { "<leader>gm",  "<cmd>Gdiffsplit!<cr>",        desc = "3-way merge (conflicts)" },
 
-		-- Diff and blame
-		{ "<leader>gd", "<cmd>Gvdiffsplit<cr>", desc = "Git diff (vertical split)" },
-		{ "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
+                -- Diff and blame
+                { "<leader>gd",  "<cmd>Gvdiffsplit<cr>",        desc = "Git diff (vertical split)" },
+                { "<leader>gb",  "<cmd>Git blame<cr>",          desc = "Git blame" },
 
-		-- Helpers
-		{ "<leader>gw", "<cmd>Gwrite<cr>", desc = "Git add current file" },
-		{ "<leader>gr", "<cmd>Gread<cr>", desc = "Git checkout current file" },
-	},
+                -- Helpers
+                { "<leader>gw",  "<cmd>Gwrite<cr>",             desc = "Git add current file" },
+                -- { "<leader>gr",  "<cmd>Gread<cr>",              desc = "Git checkout current file" },
+        },
 }
